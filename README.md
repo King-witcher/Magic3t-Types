f647c869d0f694d3ed75dc434f616b0336999555
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-07-22 16:32:04 -0300

    feat: include MatchDto

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# magic3t-types
+# Magic3T Types
+
+[![.github/workflows/publish.yml](https://github.com/King-witcher/Magic3t-Types/actions/workflows/publish.yml/badge.svg)](https://github.com/King-witcher/Magic3t-Types/actions/workflows/publish.yml)
+
+This is the package where shared types between all Magic3T projects are defined.
+
+Only types are allowed here and classes, objects or constants should never live inside this package. For enums, always use const enums.
+
+The repository is set to automatically publish new versions on committing to main.
--- common/game.ts
+++ common/game.ts
@@ -1,6 +1,53 @@
+import { League } from './rating'
+
 export type Choice = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
 export const enum Team {
   Order = 0,
   Chaos = 1,
+}
+
+export interface MatchDtoTeam {
+  id: string
+  nickname: string
+  league: League
+  division: number | null
+  lpGain: number
+  matchScore: number
+}
+
+export const enum MatchEventType {
+  Choice = 0,
+  Forfeit = 1,
+  Timeout = 2,
+  Message = 3,
+}
+
+type BaseMatchEvent = {
+  event: MatchEventType
+  side: Team
+  time: number
+}
+
+export type MatchDtoEvent = BaseMatchEvent &
+  (
+    | {
+        event: MatchEventType.Choice
+        choice: Choice
+      }
+    | {
+        event: MatchEventType.Message
+        message: string
+      }
+    | {
+        event: MatchEventType.Timeout | MatchEventType.Forfeit
+      }
+  )
+
+export interface MatchDto {
+  id: string
+  teams: Record<Team, MatchDtoTeam>
+  events: MatchDtoEvent[]
+  winner: Team | null
+  time: number
 }
